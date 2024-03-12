5970925886ca388ebca91450f7b1431e257cf8bd
wendyf30 <144722320+wendyf30@users.noreply.github.com>
2024-03-12 18:16:11 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function myFunction()
+{
+  document.getElementById("greet").style.color = "blue";
+}
+function Night(){
+  document.body.style.backgroundColor="purple";
+}
+function paragraph() {
+  document.getElementById("hi").style.color = "pink";
+}
+
